6c23f24fdd6f08ceee7b81a72e5d2b468fe58f77
busySZl <szlinb@zju.edu.cn>
2018-10-28 20:32:17 +0800

    MZM

--- pyfiberamp/external_modulation_system.py
+++ pyfiberamp/external_modulation_system.py
@@ -6,9 +6,8 @@ from pyfiberamp.parameters import *
 class ExternalModulationSystem(object):
     def __init__(
         self,
-        optical_wave_length,
         mzm_v_pi_dc,
-        mzm_v_pi_rf,
+        mzm_v_pi_rf_list,
         optical_loss_coeff,
         pd_responsivity,
         laser_optical_power_out,
@@ -18,11 +17,13 @@ class ExternalModulationSystem(object):
         R_load=50,
         R_input=50,
         t=25,
+        signal_bandwith = 1e8,
+        optical_wave_length=1550,
         edfa_enable=False
     ):
         self.modulator = MachZehnderModulator(
             V_pi_dc=mzm_v_pi_dc,
-            V_pi_rf=mzm_v_pi_rf,
+            V_pi_rf_list=mzm_v_pi_rf_list,
             optical_loss_coeff=optical_loss_coeff,
             R_internal=modulator_R_internal
         )
@@ -39,6 +40,7 @@ class ExternalModulationSystem(object):
         self.RIN = laser_RIN
         self.laser_optical_power_out = laser_optical_power_out
         self.T = T0 + t
+        self.signal_bandwith = signal_bandwith
         if edfa_enable:
             pass
 
@@ -52,32 +54,32 @@ class ExternalModulationSystem(object):
         system_output_info_dict = {}
 
         P_rf_mzm = Power_rf_input * self.R_input / (self.R_input + self.r_internal)
-        optical_power_list = self.modulator.optical_power_output(
+        optical_power_array = self.modulator.optical_power_output(
             optical_power_input=self.laser_optical_power_out,
             V_dc_bias=V_dc_bias,
             P_rf_mzm=P_rf_mzm
         )
-        current_output_list = self.photonic_detector.current_output(
-            optical_power_input=optical_power_list
+        current_output_array = self.photonic_detector.current_output(
+            optical_power_input=optical_power_array
         )
-        cnt_rf = len(current_output_list)
+        photonic_current = current_output_array / 2.0
+        P_rf_ouput = pow(photonic_current, 2) * self.R_load * np.array([1, 0.5, 0.5, 0.5])
 
-        P_rf_ouput_list = []
-        for i in range(cnt_rf):
-            photonic_current = current_output_list[i] * 0.5
-            if i == 0:
-                P_rf_ouput = photonic_current * photonic_current * self.R_load
-            else:
-                P_rf_ouput = photonic_current * photonic_current * self.R_load * 0.5
-            P_rf_ouput_list.append(P_rf_ouput)
+        rf_gain = P_rf_ouput[:, 1] / P_rf_mzm
 
-        rf_gain = P_rf_ouput_list[1] / P_rf_mzm
+        N_th_no_B = K * self.T
+        N_shot_no_B = 2 * q * photonic_current[:, 0] * 0.25 * np.sqrt(2) * self.R_load
+        RIN = 10 * np.log10(self.RIN / 10)
+        N_rin_no_B = 0.25 * pow(photonic_current[:, 0], 2) * RIN * self.R_load
 
-        N_th = self.T
-        N_shot = 2 * q * self.current_output_list[0] * 0.25
+        # get thermal noise gain
+        N_out_no_B = rf_gain * N_th_no_B + N_th_no_B + N_shot_no_B + N_rin_no_B
+
+        NF = 10 * np.log10(N_out_no_B / (N_th_no_B * rf_gain))
 
         system_output_info_dict["P_rf_ouput_list"] = P_rf_ouput_list
         system_output_info_dict["rf_gain"] = rf_gain
+        system_output_info_dict["NF"] = NF
 
         return system_output_info_dict
 
@@ -86,12 +88,12 @@ class MachZehnderModulator(object):
     def __init__(
         self,
         V_pi_dc,
-        V_pi_rf,
+        V_pi_rf_list,
         optical_loss_coeff,
         R_internal=50
     ):
         self.v_pi_dc = V_pi_dc
-        self.v_pi_rf = V_pi_rf
+        self.v_pi_rf_list =  V_pi_rf_list
         self.optical_loss_coeff = optical_loss_coeff
         self.r_internal = R_internal
 
@@ -101,19 +103,26 @@ class MachZehnderModulator(object):
         :param optical_power_input:
         :param V_dc_bias:
         :param P_rf_mzm:
-        :return:
+        :return: np.array
         """
         V_rf = np.sqrt(P_rf_mzm * 2 * self.r_internal)
 
         m_dc = np.pi / self.v_pi_dc * V_dc_bias
-        m_rf = np.pi / self.v_pi_rf * V_rf
         k = 0.5 * self.optical_loss_coeff * optical_power_input
-        p_dc = k * (1 + np.cos(m_dc)) * jn(0, m_rf)
-        p_1st = - k * 2 * np.sin(m_dc) * jn(1, m_rf)
-        p_2nd = - k * 2 * np.cos(m_dc) * jn(2, m_rf)
-        p_3rd = k * 2 * np.sin(m_dc) * jn(3, m_rf)
+        p_out = []
+        for i in range(len(self.v_pi_rf_list)):
+            m_rf = np.pi / self.v_pi_rf_list[i] * V_rf
+            p_dc = k * (1 + np.cos(m_dc)) * jn(0, m_rf)
+            p_1st = - k * 2 * np.sin(m_dc) * jn(1, m_rf)
+            p_2nd = - k * 2 * np.cos(m_dc) * jn(2, m_rf)
+            p_3rd = k * 2 * np.sin(m_dc) * jn(3, m_rf)
+            p_out.append([p_dc, p_1st, p_2nd, p_3rd])
 
-        return [p_dc, p_1st, p_2nd, p_3rd]
+        res_out = np.array(p_out, dtype=np.float32)
+        if len(self.v_pi_rf_list) == 1:
+            res_out = res_out[np.newaxis, :]
+
+        return res_out
 
 
 class PhotonicDetector(object):
@@ -137,16 +146,30 @@ class PhotonicDetector(object):
         :param optical_power_input: list
         :return:
         """
-        assert isinstance(optical_power_input, list),\
-            "Optical_power_input must be a list"
-
-        current = []
-        num_input = len(optical_power_input)
-        for i in range(num_input):
-            i = optical_power_input[i] * self.responsivity
-            current.append(i)
+        assert len(optical_power_input.shape) == 2 and optical_power_input.shape[1] == 4, \
+            "optical_power_input.shape error: {}".format(optical_power_input.shape)
+        current = optical_power_input * self.responsivity
 
         return current
+
+
+def test_example():
+    ems = ExternalModulationSystem(
+        mzm_v_pi_dc=,
+        mzm_v_pi_rf=,
+        optical_loss_coeff=,
+        pd_responsivity=,
+        laser_optical_power_out=,
+        laser_RIN= ,
+        modulator_R_internal=50,
+        pd_R_internal=50,
+        R_load=50,
+        R_input=50,
+        t=25,
+        optical_wave_length=1550,
+        edfa_enable=False
+    )
+
 
 
 
